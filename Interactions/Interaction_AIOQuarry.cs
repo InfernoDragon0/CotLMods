9543461ce5e49d063e636b108a2dd3e12109c26c
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-12-13 12:41:32 +0800

    feat: AIO Quarry Miner [1.1.3]

---
+++ Interactions/Interaction_AIOQuarry.cs
@@ -0,0 +1,117 @@
+﻿using CotLTemplateMod;
+using Lamb.UI.FollowerSelect;
+using Lamb.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using src.Extensions;
+using Lamb.UI.FollowerInteractionWheel;
+using DG.Tweening;
+using CotLMiniMods.Structures.Mines;
+
+namespace CotLMiniMods.Interactions
+{
+    internal class Interaction_AIOQuarry : Interaction
+    {
+        public Structure Structure;
+        private bool Activating = false;
+        private GameObject Player;
+        private float Delay = 0.04f;
+        public float DistanceToTriggerDeposits = 5f;
+
+        public StructuresData StructureInfo => this.Structure.Structure_Info;
+        public Structures_AIOQuarry AIOQuarry => this.Structure.Brain as Structures_AIOQuarry;
+        public override void GetLabel()
+        {
+            int count = this.StructureInfo.Inventory.Count;
+            //this.Interactable = count > 0;
+            this.SecondaryLabel = "Select Item to Quarry (Currently " + this.AIOQuarry.SelectedQuarryItem + ")";
+
+            if (this.Structure != null)
+                this.label = "Collect Items: x" + this.StructureInfo.Inventory.Count;
+            else
+                this.label = "structure was null";
+        }
+
+        private void Start()
+        {
+            this.ContinuouslyHold = true;
+            this.HasSecondaryInteraction = true;
+        }
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            Plugin.Log.LogInfo("I am enabled AIO Quarry Interaction");
+            Structure = GetComponentInParent<Transform>().GetComponent<Structure>();
+        }
+
+        public override void OnInteract(StateMachine state)
+        {
+            if (this.Activating) return;
+            base.OnInteract(state);
+            this.Activating = true;
+        }
+
+        public override void OnSecondaryInteract(StateMachine state)
+        {
+            base.OnSecondaryInteract(state);
+            List<InventoryItem> QuarryItems = this.AIOQuarry.QuarryItems;
+            state.CURRENT_STATE = StateMachine.State.InActive;
+            state.facingAngle = Utils.GetAngle(state.transform.position, this.transform.position);
+            CameraFollowTarget cameraFollowTarget = CameraFollowTarget.Instance;
+            cameraFollowTarget.SetOffset(new Vector3(0.0f, 4.5f, 2f));
+            cameraFollowTarget.AddTarget(this.gameObject, 1f);
+            HUD_Manager.Instance.Hide(false, 0);
+            
+            UIItemSelectorOverlayController itemSelector = MonoSingleton<UIManager>.Instance.ShowItemSelector(QuarryItems, new ItemSelector.Params()
+            {
+                Key = "seed_shop",
+                Context = ItemSelector.Context.Buy,
+                Offset = new Vector2(0.0f, 150f),
+                ShowEmpty = true,
+                RequiresDiscovery = false,
+                HideQuantity = true
+            });
+            itemSelector.OnItemChosen += chosenItem =>
+            {
+                this.AIOQuarry.SelectedQuarryItem = chosenItem;
+            };
+            itemSelector.OnCancel += () => HUD_Manager.Instance.Show(0);
+            itemSelector.OnHidden += () =>
+            {
+                cameraFollowTarget.SetOffset(Vector3.zero);
+                cameraFollowTarget.RemoveTarget(this.gameObject);
+                state.CURRENT_STATE = StateMachine.State.Idle;
+                itemSelector = null;
+                this.Interactable = true;
+                this.HasChanged = true;
+            };
+
+        }
+        public override void Update()
+        {
+            if ((this.Player = GameObject.FindWithTag("Player")) == null)
+                return;
+            
+            this.GetLabel();
+
+            if (this.Activating && (this.StructureInfo.Inventory.Count <= 0 || InputManager.Gameplay.GetInteractButtonUp()))
+            {
+                this.Activating = false;
+            }
+
+            if ((double)(this.Delay -= Time.deltaTime) >= 0.0 || !this.Activating)
+                return;
+
+            InventoryItem.ITEM_TYPE itemType = (InventoryItem.ITEM_TYPE)this.StructureInfo.Inventory[0].type;
+            AudioManager.Instance.PlayOneShot("event:/followers/pop_in", this.gameObject);
+            ResourceCustomTarget.Create(this.state.gameObject, this.transform.position, itemType, () => this.GiveItem(itemType));
+            this.StructureInfo.Inventory.RemoveAt(0);
+            this.Delay = 0.04f;
+        }
+
+        private void GiveItem(InventoryItem.ITEM_TYPE type) => Inventory.AddItem((int)type, 1);
+    }
+}
---
+++ Structures/Mines/Structures_AIOQuarry.cs
@@ -0,0 +1,70 @@
+﻿using COTL_API.CustomStructures;
+using COTL_API.Helpers;
+using CotLMiniMods.CCommands.Tasks;
+
+using CotLTemplateMod;
+using CotLTemplateMod.CustomFollowerCommands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace CotLMiniMods.Structures.Mines
+{
+    internal class Structures_AIOQuarry : CustomStructure, ITaskProvider
+    {
+        public override string InternalName => "Structures_AIOQuarry";
+        public override Sprite Sprite => TextureHelper.CreateSpriteFromPath(Path.Combine(Plugin.PluginPath, "Assets/crystalmines.png"));
+        public override int BuildDurationMinutes => 30;
+
+        public override string GetLocalizedName() => "AIO Quarry";
+        public override string GetLocalizedDescription() => "A machine powered by Strange Energy to generate your preferred output.";
+
+        public int ResourceMax = 1000;
+
+        public bool CheckOverrideComplete() => true;
+
+        public override List<StructuresData.ItemCost> Cost => new()
+        {
+            new(InventoryItem.ITEM_TYPE.BLACK_GOLD, 1500),
+            new(Plugin.StrangeMaterialItem, 20),
+        };
+
+        public List<InventoryItem> QuarryItems => new()
+        {
+            new(InventoryItem.ITEM_TYPE.BLACK_GOLD),
+            new(InventoryItem.ITEM_TYPE.STONE),
+            new(InventoryItem.ITEM_TYPE.STONE_REFINED),
+            new(InventoryItem.ITEM_TYPE.LOG),
+            new(InventoryItem.ITEM_TYPE.LOG_REFINED),
+            new(InventoryItem.ITEM_TYPE.SHELL),
+            new(InventoryItem.ITEM_TYPE.POOP),
+            new(InventoryItem.ITEM_TYPE.GRASS),
+            new(InventoryItem.ITEM_TYPE.MEAT),
+            new(InventoryItem.ITEM_TYPE.SEED_BEETROOT),
+            new(InventoryItem.ITEM_TYPE.SEED_MUSHROOM),
+            new(InventoryItem.ITEM_TYPE.SEED_CAULIFLOWER),
+            new(InventoryItem.ITEM_TYPE.WHEAT),
+            new(InventoryItem.ITEM_TYPE.SEED_PUMPKIN),
+            new(InventoryItem.ITEM_TYPE.SEED_FLOWER_WHITE),
+
+        };
+
+        public InventoryItem.ITEM_TYPE SelectedQuarryItem = InventoryItem.ITEM_TYPE.STONE;
+
+        public void GetAvailableTasks(ScheduledActivity activity, SortedList<float, FollowerTask> sortedTasks)
+        {
+            if (activity != ScheduledActivity.Work || ReservedForTask || Data.Inventory.Count >= ResourceMax)
+                return;
+
+            FollowerTask_AIOMiner taskResourceStation = new FollowerTask_AIOMiner(Data.ID);
+            sortedTasks.Add(taskResourceStation.Priorty, taskResourceStation);
+        }
+
+        public FollowerTask GetOverrideTask(FollowerBrain brain)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
