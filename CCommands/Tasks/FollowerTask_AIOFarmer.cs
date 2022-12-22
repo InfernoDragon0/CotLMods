e20d1ecf8a747ccdc06c8ff506db123fe7b9e5a6
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-12-22 17:35:40 +0800

    feat: Incubation Chamber [1.1.4]

---
+++ CCommands/Tasks/FollowerTask_AIOFarmer.cs
@@ -0,0 +1,150 @@
+﻿using COTL_API.Tasks;
+using CotLMiniMods.Structures.Mines;
+using CotLTemplateMod;
+using Spine;
+using UnityEngine;
+
+namespace CotLMiniMods.CCommands.Tasks
+{
+    internal class FollowerTask_AIOFarmer : CustomTask
+    {
+        public override string InternalName => "FollowerTask_AIOFarmer";
+        private int _resourceStationID;
+        private Structures_AIOFarmer _resourceStation;
+        public override int UsingStructureID => this._resourceStationID;
+        public override bool BlockSocial => true;
+        public override FollowerLocation Location => this._resourceStation.Data.Location;
+        public override float Priorty => 20f;
+
+        public FollowerTask_AIOFarmer(int resourceStationID)
+        {
+            this._resourceStationID = resourceStationID;
+            this._resourceStation = StructureManager.GetStructureByID<Structures_AIOFarmer>(this._resourceStationID);
+        }
+
+        public FollowerTask_AIOFarmer()
+        {
+            foreach (StructureBrain structureBrain in StructureManager.StructuresAtLocation(FollowerLocation.Base))
+            {
+                if (structureBrain is Structures_AIOFarmer && !structureBrain.ReservedForTask)
+                {
+                    this._resourceStationID = structureBrain.Data.ID;
+                    this._resourceStation = StructureManager.GetStructureByID<Structures_AIOFarmer>(this._resourceStationID);
+                    return;
+                }
+                    
+            }
+
+            this.End();
+        }
+
+        public override PriorityCategory GetPriorityCategory(
+          FollowerRole FollowerRole,
+          WorkerPriority WorkerPriority,
+          FollowerBrain brain)
+        {
+            switch (FollowerRole)
+            {
+                case FollowerRole.Worshipper:
+                case FollowerRole.Farmer:
+                case FollowerRole.Monk:
+                    return PriorityCategory.Low;
+                case FollowerRole.Lumberjack:
+                case FollowerRole.Worker:
+                    return PriorityCategory.WorkPriority;
+                default:
+                    return PriorityCategory.Low;
+            }
+        }
+
+        public override int GetSubTaskCode() => this._resourceStationID;
+
+        public override void ClaimReservations()
+        {
+
+            if (this._resourceStation == null)
+                return;
+            this._resourceStation.ReservedForTask = true;
+        }
+
+        public override void ReleaseReservations()
+        {
+            if (this._resourceStation == null)
+                return;
+            this._resourceStation.ReservedForTask = false;
+        }
+
+        public override Vector3 UpdateDestination(Follower follower)
+        {
+            return this._resourceStation.Data.Position + new Vector3(1.0f, 0.0f);
+        }
+        public override void Setup(Follower follower)
+        {
+            base.Setup(follower);
+            follower.SimpleAnimator.ChangeStateAnimation(StateMachine.State.Idle, "Farming-water");
+            Plugin.Log.LogInfo("the follower " + follower.Brain._directInfoAccess.Name + " is working on a AIO farm");
+        }
+
+        public override void OnStart()
+        {
+            TimeManager.OnNewPhaseStarted += new System.Action(this.OnNewPhaseStarted);
+            this.SetState(FollowerTaskState.GoingTo);
+
+        }
+        public override void OnArrive()
+        {
+            this.SetState(FollowerTaskState.Idle);
+        }
+
+        public override void TaskTick(float deltaGameTime)
+        {
+            
+            if (this.State != FollowerTaskState.Idle && this.State != FollowerTaskState.Doing)
+                return;
+
+            this._resourceStation.Data.Progress += deltaGameTime * this._brain.Info.ProductivityMultiplier;
+
+            if (this._resourceStation.Data.Progress < 35)
+                return;
+
+            this._resourceStation.Data.Progress = 0.0f;
+
+            if (this._resourceStation.Data.Inventory.Count >= this._resourceStation.ResourceMax)
+                return;
+
+            this._resourceStation.Data.Inventory.Add(new InventoryItem(this._resourceStation.SelectedQuarryItem));
+
+            Follower followerById = FollowerManager.FindFollowerByID(this._brain.Info.ID);
+            followerById.TimedAnimation("Reactions/react-laugh", 3.33f, () =>
+            {
+
+            });
+        }
+
+        public override void Cleanup(Follower follower)
+        {
+            base.Cleanup(follower);
+            follower.SimpleAnimator.ResetAnimationsToDefaults();
+            follower.SetHat(HatType.None);
+        }
+
+
+        public override void OnIdleBegin(Follower follower)
+        {
+            base.OnIdleBegin(follower);
+            follower.SetHat(HatType.Farm);
+            follower.State.facingAngle = Utils.GetAngle(follower.transform.position, this._resourceStation.Data.Position);
+
+
+        }
+
+        public override void OnDoingBegin(Follower follower)
+        {
+            base.OnDoingBegin(follower);
+            follower.SetHat(HatType.Farm);
+            follower.State.facingAngle = Utils.GetAngle(follower.transform.position, this._resourceStation.Data.Position);
+
+        }
+        private void OnNewPhaseStarted() => this.End();
+    }
+}
---
+++ Structures/Mines/Structures_AIOFarmer.cs
@@ -0,0 +1,65 @@
+﻿using COTL_API.CustomStructures;
+using COTL_API.Helpers;
+using CotLMiniMods.CCommands.Tasks;
+
+using CotLTemplateMod;
+using CotLTemplateMod.CustomFollowerCommands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace CotLMiniMods.Structures.Mines
+{
+    internal class Structures_AIOFarmer : CustomStructure, ITaskProvider
+    {
+        public override string InternalName => "Structures_AIOFarmer";
+        public override Sprite Sprite => TextureHelper.CreateSpriteFromPath(Path.Combine(Plugin.PluginPath, "Assets/aio.png"));
+        public override int BuildDurationMinutes => 30;
+
+        public override string GetLocalizedName() => "Incubation Chamber";
+        public override string GetLocalizedDescription() => "A machine powered by Strange Energy to incubate seeds.";
+
+        public int ResourceMax = 1000;
+
+        public bool CheckOverrideComplete() => true;
+
+        public override List<StructuresData.ItemCost> Cost => new()
+        {
+            new(InventoryItem.ITEM_TYPE.BLACK_GOLD, 1000),
+            new(Plugin.StrangeMaterialItem, 15),
+        };
+
+        public List<InventoryItem.ITEM_TYPE> QuarryItems => new()
+        {
+            InventoryItem.ITEM_TYPE.BLACK_GOLD,
+            InventoryItem.ITEM_TYPE.STONE,
+            InventoryItem.ITEM_TYPE.STONE_REFINED,
+            InventoryItem.ITEM_TYPE.LOG,
+            InventoryItem.ITEM_TYPE.LOG_REFINED,
+            InventoryItem.ITEM_TYPE.SHELL,
+            InventoryItem.ITEM_TYPE.POOP,
+            InventoryItem.ITEM_TYPE.GRASS,
+            InventoryItem.ITEM_TYPE.MEAT,
+            InventoryItem.ITEM_TYPE.WHEAT,
+
+        };
+
+        public InventoryItem.ITEM_TYPE SelectedQuarryItem = InventoryItem.ITEM_TYPE.STONE;
+
+        public void GetAvailableTasks(ScheduledActivity activity, SortedList<float, FollowerTask> sortedTasks)
+        {
+            if (activity != ScheduledActivity.Work || ReservedForTask || Data.Inventory.Count >= ResourceMax)
+                return;
+
+            FollowerTask_AIOFarmer taskResourceStation = new FollowerTask_AIOFarmer(Data.ID);
+            sortedTasks.Add(taskResourceStation.Priorty, taskResourceStation);
+        }
+
+        public FollowerTask GetOverrideTask(FollowerBrain brain)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
